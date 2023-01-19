cb370ec01034766820bb4e1e7ff0108ff7b7e9f8
Deyson19 <deiandresvent@hotmail.com>
2023-01-19 11:50:20 -0500

    Desarrollando la logica de acceso a datos en las capas datos y negocio.

---
+++ Sol_Minimarket.Datos/D_Categorias.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Sol_Minimarket.Entidades;
+
+namespace Sol_Minimarket.Datos
+{
+    public class D_Categorias
+    {
+        public D_Categorias() { }
+
+        public DataTable Listado_ca (string cTexto)
+        {
+            SqlDataReader resultado;
+            DataTable dt = new DataTable();
+            SqlConnection SqlConn = new SqlConnection();
+
+            try
+            {
+                SqlConn = Conexion.getInstance().CrearConexion();
+                SqlCommand comando= new SqlCommand("USP_Listado_ca", SqlConn);
+                comando.CommandType = CommandType.StoredProcedure;
+                comando.Parameters.Add("@cTexto",SqlDbType.VarChar).Value= cTexto;
+                SqlConn.Open();
+                resultado = comando.ExecuteReader();
+                dt.Load(resultado);
+                return dt;
+            }
+            catch (SqlException ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                if (SqlConn.State==ConnectionState.Open) { SqlConn.Close();}
+            }
+        }
+    }
+}
---
+++ Sol_Minimarket.Negocio/N_Categorias.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Sol_Minimarket.Entidades;
+using Sol_Minimarket.Datos;
+
+namespace Sol_Minimarket.Negocio
+{
+    public class N_Categorias
+    {
+        public N_Categorias() { }
+        public static DataTable Listado_ca(string cTexto)
+        {
+            D_Categorias datos = new D_Categorias();
+            return datos.Listado_ca(cTexto);
+        }
+
+    }
+}
